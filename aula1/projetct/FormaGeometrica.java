21c19f30da60db089a9fbdbbf375074cb3831008
Eduardo Butzen <eduardo.butzen@ufn.edu.br>
2026-07-29 11:47:10 -0300

    Create FormaGeometrica.java

---
+++ aula1/projetct/FormaGeometrica.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class FormaGeometrica {
+    
+    private float lado;
+    protected float area;
+
+    public float getLado() {
+        return lado;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+    
+    
+    public float calcularArea(){
+        
+        area = lado * lado;
+        return area;
+     
+    }
+    
+}
